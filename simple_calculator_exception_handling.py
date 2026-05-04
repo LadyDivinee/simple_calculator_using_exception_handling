fad375f0d0e5f48a53432dca79a7dfb15f4eefdf
LadyDivinee <ladydivineortega14@gmail.com>
2026-05-03 23:35:01 -0700

    parent class constructor

---
+++ simple_calculator_exception_handling.py
@@ -0,0 +1,5 @@
+#parent class constructor
+class Calculator:
+    def __init__(self, first_number, second_number):
+        self.first_number = first_number
+        self.second_number = second_number
